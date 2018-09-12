d389598ab3272ffff7b2b3ed5e3f8603a5b4eb4d
JefRousseaux <42477336+JefRousseaux@users.noreply.github.com>
2018-09-12 13:35:22 -0400

    Create page_accueil.php

---
+++ page_accueil.php
@@ -0,0 +1,80 @@
+<?php
+
+
+?>
+<!DOCTYPE html> 
+<html>
+
+<head>
+	<title>Jef Twitch</title>
+	<link rel="shortcut icon" sizes="16x16" href="https://cdn1.iconfinder.com/data/icons/micon-social-pack/512/twitch-512.png">
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css" />
+
+</head>
+
+<body>
+<header>
+<table class="imageentete" width="100%" height="50%">
+	<tr>
+		<td>
+			<img src="https://www.esports.com/upload/articles/479/image/FortniteBiling-back.jpg" width="100%" height="100%" 
+			class="imageentete">
+		</td>
+
+	</tr>
+
+</table>
+
+<table class="logo" >
+	<tr>
+		<td>
+			<img src="logo_twitch.png" width="30%" height="30%" 
+			class="logoentete">
+		</td>
+
+	</tr>
+
+</table>
+
+<div align="center"><p>Jef Rousseaux</p> <p>un <i>streamer</i> noob</p></div>
+
+<table class="index" width="100%">
+	<tr>
+
+			<td class="border" onmouseover="this.style.cursor='pointer'" align="center">
+
+			 <a href="needconnexion.php" class="border">	S'abonner </a>
+			 	</td> 
+
+
+		<td class="border" onmouseover="this.style.cursor='pointer'" align="center">
+			<a href="" class="border">Mon twitch</a>		
+		</td>
+
+		<td class="border" onmouseover="this.style.cursor='pointer'" align="center">
+			<a href="inscription.php" class="border">S'inscrire</a>
+		</td>
+
+		<td class="border" onmouseover="this.style.cursor='pointer'" align="center">
+			<a href="" class="border">Mes clips</a>
+		</td>
+
+		<td class="border" onmouseover="this.style.cursor='pointer'" align="center">
+			<a href="" class="border">Forum</a>
+		</td>
+	</tr>
+</table>
+
+</header>
+
+<table class="etat">
+	<tr>
+		<td>
+			<img src="http://anousdeux04.a.n.pic.centerblog.net/0_6bf18_517b2337_M.png" title="Déconnecter" height="45px" width="45px"></a>
+		</td>
+	</tr>
+</table>
+</body>
+
+</html>
